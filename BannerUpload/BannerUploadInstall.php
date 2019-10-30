a7c5f0de9b14c5ca8ab14326518b1ffdac25fbae
Andrew Henry <andrew_henry@trendmicro.com>
2019-10-30 14:48:50 -0400

    Added all banner functionality

--- BannerUpload/BannerUploadInstall.php
+++ BannerUpload/BannerUploadInstall.php
@@ -14,14 +14,29 @@
         "name" => "id_member",
         "type" => "mediumint",
         "size" => 8,
-        "null" => false,
+        "null" => true,
         "unsigned" => true,
     );
     
-    $file_path_column = array(
-        "name" => "file_path",
+    $file_name_column = array(
+        "name" => "file_name",
         "type" => "text",
         "null" => false
+    );
+    
+    $fallback_name_column = array(
+        "name" => "fallback_name",
+        "type" => "text",
+        "null" => true
+    );
+    
+    $upload_time_column = array(
+        "name" => "upload_time",
+        "type" => "int",
+        "size" => 10,
+        "null" => false,
+        "default" => 0,
+        "unsigned" => true
     );
     
     $id_index = array(
@@ -29,8 +44,10 @@
         "columns" => array("id_banner")
     );
     
-    $columns = array($id_column, $member_column, $file_path_column);
+    $columns = array($id_column, $member_column, $file_name_column);
     $indices = array($id_index);
     
-    $smcFunc["db_create_table"]("banner_uploads", $columns, $indices, array(), "ignore");
+    $smcFunc["db_create_table"]("banners", $columns, $indices, array(), "ignore");
+    $smcFunc["db_add_column"]("banners", $fallback_name_column, array(), "update");
+    $smcFunc["db_add_column"]("banners", $upload_time_column, array(), "update");
 ?>
